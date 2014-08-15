03975213aaa8b4260c7fef346ac17bebaf7c863c
Jonathan Marshall <jmarshall@never.you.mind>
2014-08-15 12:52:25 +1200

    use markdown and tidy up the layout a bit

--- server.R
+++ server.R
@@ -4,7 +4,13 @@ get_xy <- function(n)
 {
   x <- (rnorm(n, 0, 0.2) + 1:n) / (n+1)
   y <- rnorm(n, 0, 1)
-  list(x=x-mean(x), y=y)
+  # get rid of residual variation in y
+  x <- x - mean(x)
+
+  b <- coef(lm(y ~ x))[2]
+  y <- y - b*x
+
+  list(x=x, y=y)
 }
 
 # Define server logic required to draw a histogram
@@ -26,7 +32,6 @@ shinyServer(function(input, output) {
       {
         current_n  <<- input$n
         current_xy <<- get_xy(current_n)
-        cat("n updated to", current_n, "\n")
       }
       x <- current_xy$x
       y <- input$beta*x + current_xy$y * input$eta
@@ -34,8 +39,26 @@ shinyServer(function(input, output) {
       list(x=x, y=y)
     })
 
+  output$fdist <- renderPlot({
+    f <- anova(lm(y ~ -1 + x, data=get_current()))$F[1]
+    n <- input$n; p <- 1
+    xm <- qf(c(0.5, 0.95, 0.9999), p, n-p-1)
+    xm[3] <- max(xm[3], f)
+    x <- seq(xm[1],xm[3],length.out=100)
+    y <- df(x, p, n-p-1)
+    par(mai=c(0.5,0.1,0.1,0.1))
+    plot(NULL,xlab="F", ylab="", xlim=c(0, max(x)), ylim=range(y), type="l", yaxt="n", main="")
+    if (f < xm[3]) {
+      xp <- seq(f, xm[3],length.out=100)
+      yp <- df(xp, p, n-p-1)
+      polygon(c(f,xp,xm[3]),c(0,yp,0), col="grey90", border="grey70")
+    }
+    lines(x,y)
+    abline(v=xm[2], lty="dotted")
+    abline(v=f, col="red")
+  })
+
   output$anova <- renderPrint({
-    d <- get_current();
     anova(lm(y ~ -1 + x, data=get_current()))
   })
 
@@ -48,7 +71,7 @@ shinyServer(function(input, output) {
     abline(h=0, lty="dashed")
     segments(d$x, 0, d$x, d$y, col="blue")
     points(d$x,d$y, pch=19)
-    text(0.4, -3.2, bquote(SS[y] == .(round(ss_y, 2))), col="blue", cex=1.5)
+    text(0.4, -3.2, bquote(SS[tot] == .(round(ss_y, 2))), col="blue", cex=1.5)
   })
 
   output$linePlot <- renderPlot({
--- ui.R
+++ ui.R
@@ -5,60 +5,69 @@ shinyUI(fluidPage(
 
 #  mainPanel(
 
-  fluidRow(
-      column(6,h1("The F test for linear models"),
-               p("The F test is a general way to assess whether a linear model explains a signficant portion of the variaion in the response variable."),
-               p("The idea is to partition the variation in the response into variation explained by the linear model, and left over, or residual variation."),
-               tags$ul(tags$li("If the amount explained by the model is quite large compared to the amount of residual variation, then it would have been unlikely to have arisen by chance, and so we'd conclude that the relationship observed in our sample is likely to hold for the population."), tags$li("If the amount explained by the model is small compared to the amount of residual variation, then the relationship observed in the data may have arisen by chance, so we make no conclusion regarding the population.")),
-               p("How large the variation explained by the model needs to be in order to make the above decision can be found by  computing the F-statistic"),
-              withMathJax(helpText("$$F = \\frac{SS_{model} / p}{SS_{res} / (n-p-1)}$$")),
-              p("where \\(p\\) is the number of parameters in the model (1 for simple linear regression)."),
-              p("If the residuals are normally distributed with constant variance (standard assumptions of linear regression) then it turns out that \\(F\\) will have an F distribution with \\(p\\) and \\(n-p-1\\) degrees of freedom.  This allows us to compute the P-value.")
+  titlePanel("The F test for linear models", "F-test for linear models"),
+  hr(),
+
+  sidebarLayout(
+    sidebarPanel(
+                 withMathJax(includeMarkdown("ftest.md"))
+
+    ),
+    mainPanel(
+      tabsetPanel(
+       tabPanel("Data",
+        h3("Total variation in data"),
+        plotOutput("dataPlot"),
+        p("The total variation in the data is found by finding the sum of the square distances (blue lines) between the data and the mean under the null model, where we have only an intercept (the dashed line)."),
+        p("As y depends on x, we'd expect the variation in y to increase as the slope is larger in magnitude and also increase as the amount of additional variation increases.")
+       ),
+       tabPanel("Model",
+        h3("Variation explained by the model"),
+        plotOutput("linePlot"),
+        p("The variation explained by the model is found by the sum of the square distances (red lines) between the mean (the null model, where we have only an intercept (the dashed line), and the mean under the full model (the solid line)."),
+        p("This will depend on the strength of the relationship, or the magnitude slope.")
+       ),
+       tabPanel("Residuals",
+        h3("Residual variation"),
+        plotOutput("residPlot"),
+        p("The residual variation is what is left over after explaining the model, i.e. the sum of the squared distances (red lines) between the data and the mean of the full model (the solid line)."),
+        p("This is mostly governed by the residual variation slider.  Notice that the variation explained by the model plus the residual variation add to give total variation.")
+       ),
+       tabPanel("ANOVA",
+        h3("ANOVA table"),
+        verbatimTextOutput("anova"),
+        fluidRow(column(7,
+          p("The ANOVA table shows us the computation for the F statistic, and uses the F distribution to convert it to a P-value."),
+          p("A low P-value (less than 0.05) indicates that the linear model describes significantly more variation in the response than the null model.  A high P-value (more than 0.05) indicates that the linear model doesn't describe more variation in the response as the null model, so we should prefer the null model.")
+                       ),
+                 column(5,
+                   plotOutput("fdist", height="200px")
+                       )
+                )
+       )
       ),
-      column(2,sliderInput("n",
+      wellPanel(fluidRow(
+                column(4,sliderInput("n",
                   "Size of sample data:",
-                  min = 5,
+                  min = 10,
                   max = 100,
                   step = 5,
-                  value = 10),
-               sliderInput("eta",
+                  value = 10)),
+                column(4,sliderInput("eta",
                   "Amount of residual variation:",
-                  min = 0,
+                  min = 0.05,
                   max = 1,
                   step = 0.05,
-                  value = 0.3),
-               sliderInput("beta",
+                  value = 0.3)),
+               column(4,sliderInput("beta",
                   "Magnitude of slope:",
                   min = -3,
                   max = 3,
                   step = 0.01,
                   ticks = F,
-                  value = 1)
-      ),
-      column(4,verbatimTextOutput("anova"))
-  ),
-  hr(),
-
-  fluidRow(
-      column(4,
-        h3("Total variation in data"),
-        plotOutput("dataPlot"),
-        p("The total variation in the data is found by finding the sum of the square distances (blue lines) between the data and the mean under the null model, where we have only an intercept (the dashed line)"),
-        p("As y depends on x, we'd expect the variation in y to increase as the slope is larger in magnitude and also increase as the amount of additional variation increases")
-      ),
-      column(4,
-        h3("Variation explained by the model"),
-        plotOutput("linePlot"),
-        p("The variation explained by the model is found by the sum of the square distances (red lines) between the mean (the null model, where we have only an intercept (the dashed line), and the mean under the full model (the solid line)"),
-        p("This will depend on the strength of the relationship, or the magnitude slope")
-      ),
-      column(4,
-        h3("Residual variation"),
-        plotOutput("residPlot"),
-        p("The residual variation is what is left over after explaining the model, i.e. the sum of the squared distances (red lines) between the data and the mean of the full model (the solid line)"),
-        p("This is mostly governed by the residual variation slider.  Notice that the variation explained by the model plus the residual variation add to give total variation.")
-     )
+                  value = 1)))
+      )
+    )
   )
- # )
 ))
 
